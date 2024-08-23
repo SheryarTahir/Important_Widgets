ad8dfe9bd5403df33471a5b5661cf02e98dc504d
Sher <tahirsheryar546@gmail.com>
2024-08-23 12:45:22 +0500

    Update dropdown_list.dart

--- lib/widgets/dropdown_list.dart
+++ lib/widgets/dropdown_list.dart
@@ -9,43 +9,46 @@ class DropdownList extends StatefulWidget {
 
 class _DropdownListState extends State<DropdownList> {
 
-  String selectedItem = 'Orange';
+  String selectedValue = 'Orange';
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Drop Down List'),),
+      backgroundColor: Colors.deepPurple.shade100,
+      appBar: AppBar(
+        backgroundColor: Colors.deepPurple.shade200,
+        title: Text('Drop Down List'),
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Container(
-              padding: EdgeInsets.all(10),
+              margin: EdgeInsets.all(10),
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: Colors.deepPurple.shade50,
+              ),
               height: 70,
-              // color: Colors.red,
               width: MediaQuery.of(context).size.width,
               child: DropdownButton<String>(
-                isExpanded: true,
-                value: selectedItem,
-                onChanged: (String? newvalue) {
-                  // Handle dropdown item change here
-                  setState(() {
-                    selectedItem = newvalue!;
-                  });
-                },
-                // Map the list of items to DropdownMenuItem widgets
-                items: <String>[
-                  'Orange',
-                  'Apple',
-                  'Banana',
-                  'Mango',
-                  'Grapes',
-                ].map<DropdownMenuItem<String>>((String value) {
-                  return DropdownMenuItem<String>(
-                    value: value,
-                    child: Text(value),
-                  );
-                }).toList(),
-              ),
+                value: selectedValue,
+                  isExpanded: true,
+                  items: <String>[
+                    'Orange',
+                    'Apple',
+                    'Mango',
+                    'Grapes',
+                    'Banana'
+                  ].map<DropdownMenuItem<String>>((String value){
+                    return DropdownMenuItem<String>(
+                      value: value,
+                        child: Text(value));
+                  }).toList(),
+                  onChanged: (String? newValue){
+                    setState(() {
+                      selectedValue = newValue!;
+                    });
+                  }),
             ),
           ],
         ),
@@ -53,3 +56,60 @@ class _DropdownListState extends State<DropdownList> {
     );
   }
 }
+
+
+// import 'package:flutter/material.dart';
+//
+// class DropdownList extends StatefulWidget {
+//   const DropdownList({super.key});
+//
+//   @override
+//   State<DropdownList> createState() => _DropdownListState();
+// }
+//
+// class _DropdownListState extends State<DropdownList> {
+//
+//   String selectedItem = 'Orange';
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text('Drop Down List'),),
+//       body: Center(
+//         child: Column(
+//           mainAxisAlignment: MainAxisAlignment.center,
+//           children: [
+//             Container(
+//               padding: EdgeInsets.all(10),
+//               height: 70,
+//               // color: Colors.red,
+//               width: MediaQuery.of(context).size.width,
+//               child: DropdownButton<String>(
+//                 isExpanded: true,
+//                 value: selectedItem,
+//                 onChanged: (String? newvalue) {
+//                   // Handle dropdown item change here
+//                   setState(() {
+//                     selectedItem = newvalue!;
+//                   });
+//                 },
+//                 // Map the list of items to DropdownMenuItem widgets
+//                 items: <String>[
+//                   'Orange',
+//                   'Apple',
+//                   'Banana',
+//                   'Mango',
+//                   'Grapes',
+//                 ].map<DropdownMenuItem<String>>((String value) {
+//                   return DropdownMenuItem<String>(
+//                     value: value,
+//                     child: Text(value),
+//                   );
+//                 }).toList(),
+//               ),
+//             ),
+//           ],
+//         ),
+//       ),
+//     );
+//   }
+// }
