bfe6f5f5f1b120c7e128323504930d31a21968f0
Muhammad Sheryar Tahir <tahirsheryar546@gmail.com>
2024-08-28 12:21:16 -0700

    VR

--- lib/widgets/form_widget.dart
+++ lib/widgets/form_widget.dart
@@ -8,25 +8,26 @@ class Forms1Widget extends StatefulWidget {
 }
 
 class _Forms1WidgetState extends State<Forms1Widget> {
+
   String firstname = '';
   String lastname = '';
   String email = '';
   String password = '';
-  final _formKey = GlobalKey<FormState>();
-
-  //-----------FUNCTIONS----------------
+  final _formkey = GlobalKey<FormState>();
   
-  trySubmit () {
-  final isValid = _formKey.currentState!.validate();
+  //------------FUNCTIONS---------------
+  
+  trySubmit() {
+    final isValid = _formkey.currentState!.validate();
     if(isValid) {
-      _formKey.currentState!.save();
-      submitForm();
-    } else{
+      _formkey.currentState!.save();
+      trySave();
+    } else {
       print('Error');
     }
   }
-  
-  submitForm () {
+
+  trySave() {
     print(firstname);
     print(lastname);
     print(email);
@@ -35,84 +36,197 @@ class _Forms1WidgetState extends State<Forms1Widget> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.amber.shade100,
       appBar: AppBar(
         backgroundColor: Colors.amber.shade200,
-        title: Center(child: Text('F O R M S  W I D G E T')),
+        title: Center(child: Text('F O R M  W I D G E T')),
       ),
       body: Container(
         margin: EdgeInsets.all(10),
         child: Form(
-            key: _formKey,
+            key: _formkey,
             child: Wrap(
               children: [
                 TextFormField(
                   key: ValueKey('firstname'),
                   validator: (value) {
-                    if (value.toString().isEmpty) {
-                      return 'First Name Should not be Empty';
+                    if(value.toString().isEmpty) {
+                      return 'First name should not be empty';
                     } else {
-                      null;
+                      return null;
                     }
                   },
                   onSaved: (value) {
                     firstname = value.toString();
                   },
                 ),
-                SizedBox(height: 80),
+                SizedBox(height: 100),
                 TextFormField(
                   key: ValueKey('lastname'),
                   validator: (value) {
-                    if (value.toString().isEmpty) {
-                      return 'Last Name Should not be Empty';
+                    if(value.toString().isEmpty) {
+                      return 'First name should not be empty';
                     } else {
-                      null;
+                      return null;
                     }
                   },
                   onSaved: (value) {
                     lastname = value.toString();
                   },
                 ),
-                SizedBox(height: 80),
+                SizedBox(height: 100),
                 TextFormField(
                   key: ValueKey('email'),
                   validator: (value) {
-                    if (value.toString().isEmpty) {
-                      return 'Email Should not be Empty';
+                    if(value.toString().isEmpty) {
+                      return 'First name should not be empty';
                     } else {
-                      null;
+                      return null;
                     }
                   },
-                  onSaved: (value) {
-                    email = value.toString();
-                  },
+                  onSaved: (value) => email = value.toString(),
                 ),
-                SizedBox(height: 80),
+                SizedBox(height: 100),
                 TextFormField(
                   obscureText: true,
                   key: ValueKey('password'),
                   validator: (value) {
-                    if (value.toString().length<=5) {
-                      return 'Password Should be Six characters';
+                    if(value.toString().length<=5) {
+                      return 'Password should not be less than 5 characters';
                     } else {
-                      null;
+                      return null;
                     }
                   },
-                  onSaved: (value) {
-                    password = value.toString();
-                  },
+                  onSaved: (value) => password = value.toString(),
                 ),
-                SizedBox(
-                  height: 200,
-                ),
-                Center(
-                  child: TextButton(onPressed: (){
-                    trySubmit();
-                  },
-                      child: Text('S U B M I T'),),
-                ),
+                SizedBox(height: 100),
+                TextButton(onPressed: (){
+                  trySubmit();
+                },
+                    child: Center(child: Text('SUBMIT')),),
               ],
-            )),
+            ),
+        ),
       ),
     );
   }
 }
+
+// import 'package:flutter/material.dart';
+//
+// class Forms1Widget extends StatefulWidget {
+//   const Forms1Widget({super.key});
+//
+//   @override
+//   State<Forms1Widget> createState() => _Forms1WidgetState();
+// }
+//
+// class _Forms1WidgetState extends State<Forms1Widget> {
+//   String firstname = '';
+//   String lastname = '';
+//   String email = '';
+//   String password = '';
+//   final _formKey = GlobalKey<FormState>();
+//
+//   //-----------FUNCTIONS----------------
+//
+//   trySubmit () {
+//   final isValid = _formKey.currentState!.validate();
+//     if(isValid) {
+//       _formKey.currentState!.save();
+//       submitForm();
+//     } else{
+//       print('Error');
+//     }
+//   }
+//
+//   submitForm () {
+//     print(firstname);
+//     print(lastname);
+//     print(email);
+//     print(password);
+//   }
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.amber.shade200,
+//         title: Center(child: Text('F O R M S  W I D G E T')),
+//       ),
+//       body: Container(
+//         margin: EdgeInsets.all(10),
+//         child: Form(
+//             key: _formKey,
+//             child: Wrap(
+//               children: [
+//                 TextFormField(
+//                   key: ValueKey('firstname'),
+//                   validator: (value) {
+//                     if (value.toString().isEmpty) {
+//                       return 'First Name Should not be Empty';
+//                     } else {
+//                       null;
+//                     }
+//                   },
+//                   onSaved: (value) {
+//                     firstname = value.toString();
+//                   },
+//                 ),
+//                 SizedBox(height: 80),
+//                 TextFormField(
+//                   key: ValueKey('lastname'),
+//                   validator: (value) {
+//                     if (value.toString().isEmpty) {
+//                       return 'Last Name Should not be Empty';
+//                     } else {
+//                       null;
+//                     }
+//                   },
+//                   onSaved: (value) {
+//                     lastname = value.toString();
+//                   },
+//                 ),
+//                 SizedBox(height: 80),
+//                 TextFormField(
+//                   key: ValueKey('email'),
+//                   validator: (value) {
+//                     if (value.toString().isEmpty) {
+//                       return 'Email Should not be Empty';
+//                     } else {
+//                       null;
+//                     }
+//                   },
+//                   onSaved: (value) {
+//                     email = value.toString();
+//                   },
+//                 ),
+//                 SizedBox(height: 80),
+//                 TextFormField(
+//                   obscureText: true,
+//                   key: ValueKey('password'),
+//                   validator: (value) {
+//                     if (value.toString().length<=5) {
+//                       return 'Password Should be Six characters';
+//                     } else {
+//                       null;
+//                     }
+//                   },
+//                   onSaved: (value) {
+//                     password = value.toString();
+//                   },
+//                 ),
+//                 SizedBox(
+//                   height: 200,
+//                 ),
+//                 Center(
+//                   child: TextButton(onPressed: (){
+//                     trySubmit();
+//                   },
+//                       child: Text('S U B M I T'),),
+//                 ),
+//               ],
+//             )),
+//       ),
+//     );
+//   }
+// }
